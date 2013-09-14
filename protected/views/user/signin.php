6d2fce2171b563f898db065ac4ce4ad6cca90f9f
caoluc <caothanhluc@gmail.com>
2013-09-14 17:25:22 +0700

    add bootstrap yii and generate signin form

---
+++ protected/views/user/signin.php
@@ -0,0 +1,37 @@
+<?php
+$this->breadcrumbs = array(
+    'User' => array('signin'),
+    'Log in',
+);
+$this->widget('bootstrap.widgets.TbAlert');
+?>
+<div class="center_form">
+    <div class ="row text-center">
+        <h1>Sign In</h1>
+    </div>
+    <div style="height: 50px"></div>
+    <form method="POST"> 
+        <?php echo CHtml::errorSummary($form); ?>
+        <div class="row">
+            <?php echo CHtml::avtiveTextField($form, 'username', array('class' => 'span6 offset3', 'placeholder' => 'Username', 'autofocus' => 'autofocus')); ?>
+        </div>
+        <div class="row">
+            <?php echo CHtml::activePasswordField($form, 'password', array('class' => 'span6 offset3', 'placeholder' => 'Password')); ?>
+        </div>
+        <div style="height: 20px"></div>
+        <div class="row" style="color: blue; font-size: 8px">
+            <label class="span6 offset3">
+                <?php echo CHtml::activeCheckbox($form, 'rememberMe'); ?>
+                Log in automatically
+            </label>
+        </div>
+        <div class="row">
+            <div class="span6 offset3">
+                <?php echo CHtml::link('Forget your password?', array('user/forgetPassword')); ?>                
+            </div>
+        </div>
+        <div style="height: 20px"></div>
+        <div class="row"><?php echo CHtml::submitButton('Sign in', array('class' => 'span2 offset5')); ?>
+        </div>
+    </form>
+</div>
